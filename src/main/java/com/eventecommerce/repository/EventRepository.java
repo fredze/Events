174d6aaa1d059d6c5ebe734a82f2caf566ffd6a6
fredze <zembo.alfred@gmail.com>
2018-09-11 15:29:43 +0200

    function search event query by name

--- src/main/java/com/eventecommerce/repository/EventRepository.java
+++ src/main/java/com/eventecommerce/repository/EventRepository.java
@@ -1,7 +1,11 @@
 package com.eventecommerce.repository;
 
+import com.eventecommerce.domain.Category;
 import com.eventecommerce.domain.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -11,5 +15,15 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    /**
+     * Find a events by searching with name of events
+     * @param mc
+     * @param pageable
+     * @return
+     */
+    @Query("SELECT p FROM Event p where p.name like :x")
+    Page<Event> findEventsByName(@Param("x")String mc, Pageable pageable);
+
 
 }
